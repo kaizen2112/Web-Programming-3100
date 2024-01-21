baf111206cc3069d57acb5e4d63678b5e5a80a45
tasrif <tasrif701@gmail.com>
2024-01-21 16:01:56 +0600

    Lab 2 done

---
+++ Web Programming Lab/Lab 02/script.js
@@ -0,0 +1,7 @@
+function displayButtonPressed() {
+    // console.log("Display button clicked")
+    document.getElementById("title").innerHTML = "A2 Lab-2";
+    document.getElementById("para").innerHTML = "Shei bhai";
+
+    window.alert("Display button clicked")
+}
